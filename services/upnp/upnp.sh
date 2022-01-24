dd9cdc84432ccd8a56bb8aa29a370fffed9017d5
Ryan Bennett <nomad.ry@gmail.com>
2022-01-24 11:54:31 -0700

    WIP

--- services/upnp/upnp.sh
+++ services/upnp/upnp.sh
@@ -1,13 +1,13 @@
   #!/bin/bash
 
-  LOCAL_IP=$( ip a show wlp2s0 | grep 'inet ' | awk '{ print $2 }' | cut -f1 -d'/' )
+  LOCAL_IP=$( ip a show wlan0 | grep 'inet ' | awk '{ print $2 }' | cut -f1 -d'/' )
   echo "local IP is $LOCAL_IP"
 
   GATEWAY_IP=$( ip r | grep default | awk '{ print $3}' )
 # upnpc -e "Certbot" -r 80 80 tcp
 # upnpc -e "Certbot" -r 443 443 tcp
 
-  for PORT in 8090
+  for PORT in 4443
   do
   echo "upnp $PORT"
   upnpc -a $LOCAL_IP $PORT $PORT TCP  
